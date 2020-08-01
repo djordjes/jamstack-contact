68b8b91f5b6ec1c5d40c69b0491778e33ddfdb13
Đorđe Savić <djordje.savic@tobonet.com>
2020-08-01 18:11:29 +0200

    dump env

--- functions/contact.js
+++ functions/contact.js
@@ -3,6 +3,9 @@ exports.handler = (event, _context, callback) => {
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ boop: true }),
+    body: JSON.stringify({
+      boop: true,
+      faunaKey: process.env.FAUNADB_SERVER_SECRET,
+    }),
   });
 };
