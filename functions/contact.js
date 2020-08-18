347453351029f8870a01ebc0c0a5df1e5ae1c2fe
Đorđe Savić <djordje.savic@tobonet.com>
2020-08-18 19:58:17 +0200

    testing env injecting

--- functions/contact.js
+++ functions/contact.js
@@ -1,10 +1,19 @@
 exports.handler = (event, _context, callback) => {
-  console.log({ event });
+  const headers = {
+    "Access-Control-Allow-Origin": "https://www.netlify.com/",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  };
 
   callback(null, {
     statusCode: 200,
+    headers,
     body: JSON.stringify({
       boop: true,
-      }),
+      event,
+      _context,
+      callback,
+      faunaKey: process.env.FAUNADB_SERVER_SECRET,
+    }),
   });
 };
