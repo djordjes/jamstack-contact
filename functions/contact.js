e55b56084e79a5f359bdcb6033ecf1aeb339076e
Đorđe Savić <djordje.savic@tobonet.com>
2020-08-18 20:20:10 +0200

    testing env injecting 2

--- functions/contact.js
+++ functions/contact.js
@@ -1,19 +1,11 @@
 exports.handler = (event, _context, callback) => {
-  const headers = {
-    "Access-Control-Allow-Origin": "https://www.netlify.com/",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-  };
-
   callback(null, {
     statusCode: 200,
-    headers,
     body: JSON.stringify({
       boop: true,
       event,
       _context,
       callback,
-      faunaKey: process.env.FAUNADB_SERVER_SECRET,
     }),
   });
 };
